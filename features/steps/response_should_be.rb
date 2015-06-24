c0a02b9486db09000bd9a8b41a3124be211be487
Janet Mancha <janet.mancha@gmail.com>
2015-06-24 23:20:29 +0200

    rspec

--- features/steps/response_should_be.rb
+++ features/steps/response_should_be.rb
@@ -1,15 +1,12 @@
 require 'json'
+require 'rspec'
 
 Then(/^the response should be "([^"]*)"$/) do |code|
-  if code != @response_code
-    fail
-  end
+  @response_code.should == code
 end
 
 Then(/^the response body should be$/) do |body|
   parsedBody = JSON.parse(body)
   parsedResponseBody = JSON.parse(@response_body)
-  if parsedBody != parsedResponseBody
-    fail
-  end
+  parsedResponseBody.should == parsedBody
 end
